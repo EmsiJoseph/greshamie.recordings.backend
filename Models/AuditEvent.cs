5397042561485e6a9eaa5de3e211934c08d1e1db
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-06 10:11:23 +0800

    Add AuditEntry model and seeder; remove ClarifyGoTokenHandler middleware

---
+++ Models/AuditEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models;
+
+public class AuditEntry
+{
+    public int Id { get; set; }
+    [Required] public string? UserId { get; set; }
+    [Required] public string? ActionId { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+    public string? Details { get; set; }
+    [ForeignKey("UserId")] public virtual User? User { get; set; }
+    [ForeignKey("ActionId")] public virtual AuditAction? Action { get; set; }
+}
