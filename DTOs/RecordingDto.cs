22f81f5d230b5a531e3c7f584ee2dfcd8655171d
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-06 17:51:21 +0800

    Remove Recording model and introduce RecordingDto for improved data transfer

---
+++ DTOs/RecordingDto.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.DTOs;
+
+public class RecordingDto
+{
+    public string? id { get; set; }
+    public string? caller { get; set; }
+    public string? receiver { get; set; }
+    public DateTime? startDateTime { get; set; }
+    public DateTime? endDateTime { get; set; }
+    public string? callType { get; set; }
+    public bool? isLive { get; set; }
+    public int? durationSeconds { get; set; }
+    public string? recorder { get; set; }
+    public bool? hasPciCompliance { get; set; }
+    public bool? hasVideoRecording { get; set; }
+    public bool? hasQualityEvaluation { get; set; }
+}
