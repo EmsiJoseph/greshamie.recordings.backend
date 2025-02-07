9b729fd8bc876cc4b972748a203d0b282c264e82
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-07 13:09:22 +0800

    Refactor Program.cs by removing unused services and endpoints

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
 using System.Text;
-using backend.Constants;
 using backend.Data;
-using backend.DTOs;
-using backend.Extensions;
 using backend.Middleware;
 using backend.Models;
-using backend.Services.Audits;
 using backend.Services.Auth;
 using backend.Services.ClarifyGoServices.Comments;
 using backend.Services.ClarifyGoServices.HistoricRecordings;
 using backend.Services.ClarifyGoServices.LiveRecordings;
 using backend.Services.ClarifyGoServices.Tags;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
@@ -102,8 +97,6 @@ builder.Services.AddScoped<ITagsService, TagsService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
-// 2.6 Audit Service
-builder.Services.AddScoped<IAuditService, AuditService>();
 
 // 3. HTTP Client Configurations
 var identityServerUri = configuration["ClarifyGoAPI:IdentityServerUri"]
@@ -157,152 +150,5 @@ app.UseAuthorization();
 app.UseOutputCache();
 app.UseRateLimiter();
 app.UseMiddleware<GlobalExceptionHandler>();
-
-// 6. Endpoints
-// Unprotected endpoint
-app.MapGet("/unprotected", () => Results.Ok("No auth required"));
-
-// Protected endpoint
-app.MapGet("/protected", (HttpContext context) =>
-{
-    var user = context.User;
-    return user?.Identity?.IsAuthenticated == true
-        ? Results.Ok($"Hello, {user.Identity.Name}")
-        : Results.Unauthorized();
-}).RequireAuthorization(new AuthorizeAttribute { AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme });
-
-app.MapGet("/debug-claims", (HttpContext context) =>
-{
-    if (context.User.Identity?.IsAuthenticated == true)
-    {
-        var claims = context.User.Claims.Select(c => new { c.Type, c.Value });
-        return Results.Ok(claims);
-    }
-
-    return Results.Unauthorized();
-}).RequireAuthorization();
-
-
-// 6.1. Live Recordings Endpoints
-// 6.1.1. Get All Live Recordings
-app.MapGet(AppApiEndpoints.Recordings.Live.GetAll,
-        async (ILiveRecordingsService service) => { return await service.GetLiveRecordingsAsync(); })
-    .CacheOutput("RecordingsCache")
-    .RequireAuthorization();
-
-// 6.1.2. Resume Recording
-app.MapPut(AppApiEndpoints.Recordings.Live.Resume,
-        async (ILiveRecordingsService service, string recorderId, string recordingId) =>
-        {
-            await service.ResumeRecordingAsync(recorderId, recordingId);
-            return Results.Ok();
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.1.3. Pause Recording
-app.MapPut(AppApiEndpoints.Recordings.Live.Pause,
-        async (ILiveRecordingsService service, string recorderId, string recordingId) =>
-        {
-            await service.PauseRecordingAsync(recorderId, recordingId);
-            return Results.Ok();
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.1.4. Get Comments
-app.MapGet(AppApiEndpoints.Recordings.Live.Comments,
-        async (ICommentsService service, string recordingId) =>
-        {
-            bool isLiveRecording = true;
-            await service.GetCommentsAsync(recordingId, isLiveRecording);
-        })
-    .CacheOutput("CommentsCache")
-    .RequireAuthorization();
-
-// 6.1.5. Post Comment
-app.MapPost(AppApiEndpoints.Recordings.Live.Comments,
-        async (ICommentsService service, string recordingId, string comment) =>
-        {
-            bool isLiveRecording = true;
-            await service.PostCommentAsync(recordingId, comment, isLiveRecording);
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.1.6. Delete Comment
-app.MapDelete(AppApiEndpoints.Recordings.Live.CommentById,
-        async (ICommentsService service, string recordingId, string commentId) =>
-        {
-            bool isLiveRecording = true;
-            await service.DeleteCommentAsync(recordingId, commentId, isLiveRecording);
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.1.7. Get Tags
-app.MapGet(AppApiEndpoints.Recordings.Live.Tags,
-        async (ITagsService service, string recordingId) =>
-        {
-            bool isLiveRecording = true;
-            await service.GetTagsAsync(recordingId, isLiveRecording);
-        })
-    .CacheOutput("TagsCache")
-    .RequireAuthorization();
-
-// 6.1.8. Post Tag
-app.MapPost(AppApiEndpoints.Recordings.Live.TagOperations,
-        async (ITagsService service, string recordingId, string tag) =>
-        {
-            bool isLiveRecording = true;
-            await service.PostTagAsync(recordingId, tag, isLiveRecording);
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.1.9. Delete Tag
-app.MapDelete(AppApiEndpoints.Recordings.Live.TagOperations,
-        async (ITagsService service, string recordingId, string tag) =>
-        {
-            bool isLiveRecording = true;
-            await service.DeleteTagAsync(recordingId, tag, isLiveRecording);
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-// 6.2. Historic Recordings Endpoints
-// 6.2.1. Get All Historic Recordings with default start and end dates
-app.MapGet(AppApiEndpoints.Recordings.Historic.GetAll,
-        async (IHistoricRecordingsService service,
-            [AsParameters] RecordingSearchFiltersDto searchFiltersDto) =>
-        {
-            searchFiltersDto.StartDate = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
-            searchFiltersDto.EndDate = DateTime.Now;
-            Console.WriteLine($"Start Date: {searchFiltersDto.StartDate}, End Date: {searchFiltersDto.EndDate}");
-            
-            return await service.SearchRecordingsAsync(searchFiltersDto);
-        }
-    )
-    .CacheOutput("RecordingsCache")
-    .RequireAuthorization();
-
-// 6.2.2. Get All Historic Recordings with custom start and end dates
-app.MapGet(AppApiEndpoints.Recordings.Historic.Search,
-        async (IHistoricRecordingsService service,
-                [AsParameters] RecordingSearchFiltersDto searchFiltersDto) =>
-            await service.SearchRecordingsAsync(searchFiltersDto))
-    .CacheOutput("RecordingsCache")
-    .RequireAuthorization();
-
-// 6.2.3. Delete Historic Recording
-app.MapDelete(AppApiEndpoints.Recordings.Historic.Delete,
-        async (IHistoricRecordingsService service, string recordingId) =>
-        {
-            await service.DeleteRecordingAsync(recordingId);
-        })
-    .RequireRateLimiting("PerUserPolicy")
-    .RequireAuthorization();
-
-app.UseAuthorization();
 app.MapControllers();
 app.Run();
